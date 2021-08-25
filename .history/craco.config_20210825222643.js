920ed22d99f06f7627a77e5b181a3a333fa09b78
aknessy <aknessy@yahoo.com>
2021-08-25 22:59:44 +0100

    First project commit

---
+++ .history/craco.config_20210825222643.js
@@ -0,0 +1,19 @@
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}
